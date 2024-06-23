3a74a56a6ca320557d9da5f1934497225a185123
GuillermoMaresGalindo <106297487+GuillermoMaresGalindo@users.noreply.github.com>
2024-06-23 15:29:11 -0600

    feature importance 2

--- 5_StreamlitML/app.py
+++ 5_StreamlitML/app.py
@@ -57,6 +57,7 @@ def train_model(df, algorithm='Linear Regression'):
     X = df[['longitude', 'latitude', 'housing_median_age', 'total_rooms',
        'total_bedrooms', 'population', 'households', 'median_income']]
     y = df['median_house_value']
+    feature_names = X.columns
     imputer = SimpleImputer(strategy='mean')
     X = imputer.fit_transform(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -64,7 +65,7 @@ def train_model(df, algorithm='Linear Regression'):
     model.fit(X_train, y_train)
     evaluate_model(model, X_test, y_test)
     if algorithm == 'Random Forest Regressor':
-        feature_importances = pd.DataFrame({'Feature': X.columns, 'Importance': model.feature_importances_})
+        feature_importances = pd.DataFrame({'Feature': feature_names, 'Importance': model.feature_importances_})
         feature_importances = feature_importances.sort_values(by='Importance', ascending=False)
         st.subheader('Feature Importances:')
         st.write(feature_importances)
